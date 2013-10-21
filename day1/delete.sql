1b2796e3135aee3b5659f548314c374443d918d5
shiqing881215 <shiqing881215@gmail.com>
2013-10-20 22:07:54 -0700

    Update delete.sql

--- day1/delete.sql
+++ day1/delete.sql
@@ -1,7 +1,10 @@
 --delete
 set serveroutput on
 
+DECLARE
+  v_dept_name departments.department_name%type;
 BEGIN
-  DELETE FROM departments WHERE department_id > 290;
+  DELETE FROM departments WHERE department_id > 290
+  returning department_name into v_dept_name;
   dbms_output.put_line(SQL%ROWCOUNT || ' rows deleted.');
 END;
